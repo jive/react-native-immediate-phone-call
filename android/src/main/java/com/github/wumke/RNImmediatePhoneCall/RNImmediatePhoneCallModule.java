021086dee1a9455528be0a3a271bce32775b4163
Robert Barclay <Robert.Barclay@logmein.com>
2019-10-04 12:54:52 -0600

    fixed the code so that it will now compile. fixed the imports.

--- android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallModule.java
+++ android/src/main/java/com/github/wumke/RNImmediatePhoneCall/RNImmediatePhoneCallModule.java
@@ -1,5 +1,6 @@
 package com.github.wumke.RNImmediatePhoneCall;
 
+import android.app.Activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,25 +9,21 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.PermissionListener;
 
-public class RNImmediatePhoneCallModule extends ReactContextBaseJavaModule {
+public class RNImmediatePhoneCallModule extends ReactContextBaseJavaModule implements PermissionListener {
 
-    private static RNImmediatePhoneCallModule rnImmediatePhoneCallModule;
-
-    private ReactApplicationContext reactContext;
     private Promise promise;
-    private static String number = "";
+    private String number = "";
     private static final int PERMISSIONS_REQUEST_ACCESS_CALL = 101;
 
     public RNImmediatePhoneCallModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        if (rnImmediatePhoneCallModule == null) {
-            rnImmediatePhoneCallModule = this;
-        }
-        rnImmediatePhoneCallModule.reactContext = reactContext;
     }
 
     @Override
@@ -38,58 +35,56 @@ public class RNImmediatePhoneCallModule extends ReactContextBaseJavaModule {
     public void immediatePhoneCall(String number, Promise promise) {
 
         if (number == null || number.isEmpty()) {
-            promise.reject(new JSApplicationIllegalArgumentException("Invalid number: " + url));
+            promise.reject(new JSApplicationIllegalArgumentException("Invalid number: " + number));
+            return;
+        }
+
+        Activity currentActivity = getCurrentActivity();
+        if (currentActivity == null) {
+            promise.reject(new JSApplicationIllegalArgumentException("Current Activity is null: " + number));
             return;
         }
 
         try {
-            Bool hasPermission = ContextCompat.checkSelfPermission(reactContext.getApplicationContext(),
-            android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED
-
-            Activity currentActivity = getCurrentActivity();
-            if (currentActivity == null) {
-                currentActivity = getReactApplicationContext();
-            }
+            boolean hasPermission = ContextCompat.checkSelfPermission(getReactApplicationContext().getBaseContext(),
+                    android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
 
             if (hasPermission) {
                 call(Uri.encode(number), promise, currentActivity);
             } else {
-                RNImmediatePhoneCallModule.promise = promise;
-                RNImmediatePhoneCallModule.number = Uri.encode(number);
+                this.promise = promise;
+                this.number = Uri.encode(number);
 
                 ActivityCompat.requestPermissions(currentActivity,
-                    new String[]{android.Manifest.permission.CALL_PHONE},
-                    PERMISSIONS_REQUEST_ACCESS_CALL);
+                        new String[]{android.Manifest.permission.CALL_PHONE},
+                        PERMISSIONS_REQUEST_ACCESS_CALL);
             }
         } catch(Exception e) {
-            promise.reject(new JSApplicationIllegalArgumentException("Could not open URL '" + url + "': " + e.getMessage()));
+            promise.reject(new JSApplicationIllegalArgumentException("Could not open URL '" + number + "': " + e.getMessage()));
         }
     }
-	
-	@SuppressLint("MissingPermission")
-    private static void call(String number, Promise promise, Activity activity) {
+
+    @SuppressLint("MissingPermission")
+    private void call(String number, Promise promise, Activity activity) {
         try {
             String url = "tel:" + number;
             Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url).normalizeScheme());
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             activity.startActivity(intent);
+            promise.resolve("success");
         } catch (Exception e) {
-            promise.reject(new JSApplicationIllegalArgumentException("Could not open URL '" + url + "': " + e.getMessage()));
+            promise.reject(new JSApplicationIllegalArgumentException("Could not open URL '" + number + "': " + e.getMessage()));
         }
     }
 
-    public static void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_CALL: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Activity currentActivity = getCurrentActivity();
-                    if (currentActivity == null) {
-                        currentActivity = getReactApplicationContext();  
-                    }
-                    call(RNImmediatePhoneCallModule.number, RNImmediatePhoneCallModule.promise, currentActivity);
-                }
+    @Override
+    public boolean onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_CALL) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                call(number, promise, getCurrentActivity());
+                return true;
             }
         }
+        return false;
     }
 }
