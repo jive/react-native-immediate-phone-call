5b15a9f83f605e6e57c0ba14f0367804ae4b2f29
Mathieu Tremblay <mathieu.tremblay@jive.com>
2020-07-15 23:41:46 -0400

    feat: Add canCall function GTCMOB-2436

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-import {NativeModules, Platform, PermissionsAndroid} from 'react-native';
+import { NativeModules, Platform, PermissionsAndroid } from "react-native";
 
-const isAndroid = Platform.OS === 'android';
+const isAndroid = Platform.OS === "android";
 
 const defaultTitle = "Request Phone Permission";
 const defaultMessage = "Grant access to enable calling your phone";
 
 var RNImmediatePhoneCall = {
-  immediatePhoneCall: async function(number, checkPermissions = true, title = defaultTitle, message = defaultMessage, buttonNegative = 'Cancel', buttonPositive = 'OK') {
+  immediatePhoneCall: async function (
+    number,
+    checkPermissions = true,
+    title = defaultTitle,
+    message = defaultMessage,
+    buttonNegative = "Cancel",
+    buttonPositive = "OK"
+  ) {
     if (isAndroid) {
       if (checkPermissions) {
         try {
-        
-          let permission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CALL_PHONE);
-          if(!permission) {
-            let granted = await requestCallPermission(title, message, buttonNegative, buttonPositive);
+          let permission = await PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.CALL_PHONE
+          );
+          if (!permission) {
+            let granted = await requestCallPermission(
+              title,
+              message,
+              buttonNegative,
+              buttonPositive
+            );
             if (granted) {
-              return NativeModules.RNImmediatePhoneCall.immediatePhoneCall(number);
+              return NativeModules.RNImmediatePhoneCall.immediatePhoneCall(
+                number
+              );
             }
             return Promise.reject("Permission not granted");
           }
@@ -27,10 +42,19 @@ var RNImmediatePhoneCall = {
       }
     }
     return NativeModules.RNImmediatePhoneCall.immediatePhoneCall(number);
-  }
+  },
+
+  canCall: async function () {
+    return NativeModules.RNImmediatePhoneCall.canCall();
+  },
 };
 
-async function requestCallPermission(title, message, buttonNegative, buttonPositive) {
+async function requestCallPermission(
+  title,
+  message,
+  buttonNegative,
+  buttonPositive
+) {
   try {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.CALL_PHONE,
@@ -38,8 +62,8 @@ async function requestCallPermission(title, message, buttonNegative, buttonPosit
         title,
         message,
         buttonNegative,
-        buttonPositive
-      },
+        buttonPositive,
+      }
     );
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
       return true;
